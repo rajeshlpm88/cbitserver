3b07944793b1b63f5be4d83acc7eabd6f44daa45
rajeshlpm88 <rajeshlpm88@gmail.com>
2024-08-06 13:54:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+//const cors=require('cors')
+const alienRouter = require('./routes/aliens')
+
+            
+//const url = 'mongodb+srv://rajeshlpm88:9944836316@cluster0.yzw2hpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const url = 'mongodb://127.0.0.1:27017/cbit'
+const app = express()
+mongoose.connect(url)
+const con = mongoose.connection
+con.on('open', () =>
+{
+console.log('connected...')
+})
+//app.use(cors())
+app.use(express.json())
+
+app.use('/aliens',alienRouter)
+app.listen(9000, () =>
+{
+console.log('Server started')
+})
